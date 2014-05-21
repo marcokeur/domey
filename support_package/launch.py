ff6c3e9cf71233302b4adf413fa7fd6f9cbb188b
Marco Keur <marco@precision-it.nl>
2014-05-21 07:55:43 +0200

    Update launch.py

--- support_package/launch.py
+++ support_package/launch.py
@@ -1,1 +1,14 @@
-bla
+import sys
+
+  processList = []
+
+def runScripts():
+  processList.append(Popen(['watch', 'ls']))
+  processList.append(Popen(['watch', 'df']))
+  processList.append(Popen(['watch', 'uptime']))
+
+def main():
+    runScripts()
+
+if __name__ == "__main__":
+    main()
