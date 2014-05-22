10dcb0f5b6abcd67cfa684ab492ed799c8a044e8
Marco Keur <marco@precision-it.nl>
2014-05-22 12:36:53 +0200

    Update serial_to_udp.py

--- support_package/scripts/serial_to_udp.py
+++ support_package/scripts/serial_to_udp.py
@@ -1,60 +1,37 @@
-#
-# Python script for serial to UDP communication
-# Alex Olwal, 2012 03 24
-# www.olwal.com
-# 
-
 import serial
 import sys
-
+import daemon
 from socket import *
+
+baud_rate = 115200
+udp_ip = "127.0.0.1"
+udp_port 1884
+serial_port = "/dev/ttyAMA0"
+
+s = serial.Serial( serial_port, baud_rate, timeout=1 )
 
 def send(msg, ip, port):
     socket(AF_INET,SOCK_DGRAM).sendto(msg, (ip, port))
 
-baud_rate = 115200
+def main():
+	while (1):
+        	line = s.readline()
 
-if ( len(sys.argv) == 1 ):
-        print "Serial-to-UDP utility | Alex Olwal, 2012, www.olwal.com"
-        print "Syntax: " + sys.argv[0] + " serial_port udp_ip(= 127.0.0.1) udp_port(= 5000)"
-        print "Example: " + sys.argv[0] + " COM20 127.0.0.1 5000"
-        quit()
-
-serial_port = sys.argv[1]
-
-if ( len(sys.argv) >= 3 ):
-        udp_ip = sys.argv[2]
-else:
-        udp_ip = "127.0.0.1"
-
-if ( len(sys.argv) >= 4 ):
-        udp_port = sys.argv[3]
-else:
-        udp_port = "5000"
-		
-if ( len(sys.argv) >= 5):
-        printing = 1
-else:
-		printing = 0
-
-print "Reading from serial port: " + serial_port
-print "Sending to " + udp_ip + ":" + udp_port
-
-udp_port = int(udp_port)
-
-s = serial.Serial( serial_port, baud_rate, timeout=1 )
-
-while (1):
-        line = s.readline()
-
-        if (line != ''):
-			if (printing):
-				print line[:-1]
-            
+        	if (line != ''):
 			send( line, udp_ip, udp_port )
 
-        else:
-            if (printing):
-                print "."
+def stop(signum, frame):
+	s.close()
+	sys.exit(0)
+	
+def start():
+	context = daemon.DaemonContext()
+	context.signal_map = { signal.SIGTERM: stop }
 
-s.close()
+    	context.open()
+    	
+    	with context:
+        	doMainProgram()
+
+if __name__ == "__main__":
+	start()
