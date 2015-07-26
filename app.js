29bae3703f559dbfe589c2545fb0c3d79b12f2e6
Marco Keur <marco@precision-it.nl>
2015-07-26 21:32:01 +0200

    cleanup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var winston = require('winston');
-winston.add(winston.transports.File, { filename: 'debug.log' });
+winston.add(winston.transports.File, {filename: 'debug.log'});
 
 winston.info('framework starting');
 
@@ -8,35 +8,12 @@ GLOBAL.Domey = new core();
 
 Domey.init();
 
-Domey.on('ready', function(){
+Domey.on('ready', function () {
     winston.info('initialization done!');
-    
-    var devices;
-    console.log(Domey.manager('drivers').getDriver('bulb').init(devices, function(){
-        //console.log("done");
-        
-        //var device = Manager.manager('drivers').getDriver('bulb').getDevice(1);
-        //console.log('device: ' + device.id);
-        //Manager.manager('drivers').getDriver('bulb').capabilities.onoff.set(device, true, function(state){
-               //console.log('device state set: ' + state);
-        //});
-        winston.verbose('bulb driver initialized');
-    }));
-    
-    
-    console.log(Domey.manager('drivers').getDriver('roomnode').init(devices, function(){
-        //console.log("done");
-        winston.verbose('roomnode driver initialized');
-        
-    }));    
-    
-                
-
-   
 });
 
 
-            setTimeout(function() {
-                Domey.manager('flow').trigger('measurement', { 'light': 200 });
-                console.log('trigger fired');
-            }, 3000);
+setTimeout(function () {
+    Domey.manager('flow').trigger('measurement', {'light': 200});
+    console.log('trigger fired');
+}, 3000);
