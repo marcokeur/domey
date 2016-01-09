3f4f1faa3446c3ed34ca4e2216975cf438237c91
Marco Keur <marco@precision-it.nl>
2016-01-09 18:50:57 +0100

    corrected timings

--- things/com.arc-tech/drivers/switch/driver.js
+++ things/com.arc-tech/drivers/switch/driver.js
@@ -6,19 +6,19 @@ var self = {
     switches: [
         {   deviceId:     0,
             unit:         1,
-            id:           1,
-            enabled:  false,
+            id:           2,
+            enabled:  false
         },
         {   deviceId:     1,
             unit:         1,
-            id:           1,
-            enabled:  false,
+            id:           2,
+            enabled:  false
         }
     ],
 
     init: function( devices, callback ){
 
-        var self = this;
+        self = this;
         console.log('driver kaku old init');
 
         // we're ready
@@ -75,12 +75,13 @@ var self = {
 };
 
 function sendSignal(channel, enabled, callback){
+    var kakuPeriodicity = 375;
     var opts = {
         //startSig    : new Buffer([0]),        //arc tech old doesnt have a startsig
-        highSig     : [ 295, 1180, 1180, 295],  //high third pulse, means 0
-        lowSig      : [ 295, 1180, 295, 1180],  //low third pulse, means 1
-        endSig      : [ 295, 11210],            //endsig of messages
-        cycles      : [ 20 ]                    //repeats of whole message
+        highSig     : [ kakuPeriodicity, 3*kakuPeriodicity, 3*kakuPeriodicity, kakuPeriodicity],  //high third pulse, means 0
+        lowSig      : [ kakuPeriodicity, 3*kakuPeriodicity, kakuPeriodicity, 3*kakuPeriodicity],  //low third pulse, means 1
+        endSig      : [ kakuPeriodicity, 32*kakuPeriodicity],            //endsig of messages
+        cycles      : [ 3 ]                    //repeats of whole message
     }
 
     if( enabled ){
@@ -94,7 +95,49 @@ function sendSignal(channel, enabled, callback){
     Domey.log(0, 0, 'Sending out: ' + JSON.stringify(opts));
 
     Domey.interface('wireless_433').send(opts, callback);
-
 }
 
+/*
+var code;
+function createCode(unit, id, state){
+
+    code = [];
+
+    convertToPulses(unit, 5);
+
+    convertToPulses(id, 5);
+
+    addToCode(highPulse);
+
+    convertToPulses(state);
+
+    addToCode(endPulse);
+
+    console.log(code);
+}
+
+function convertToPulses(number, length){
+    var binary = dec2bin(number);
+    console.log(binary);
+    if((binary.length - length) < 0){
+        for(var i = 0; i > (binary.length - length); i--){
+            addToCode(lowPulse);
+        }
+    }
+
+    for(var i = 0; i < binary.length; i++){
+        if(binary[i] == 1){
+            addToCode(lowPulse);
+        }else{
+            addToCode(highPulse);
+        }
+    }
+}
+
+function addToCode(pulse){
+    pulse.forEach(function(value){
+        code.push(value);
+    });
+}
+*/
 module.exports = self;
